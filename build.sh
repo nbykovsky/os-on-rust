52e8304e4ba977b9360020d907d67df5c1a277a4
Nikolay Bykovsky <nikolay.bykovsky@gmail.com>
2021-09-24 20:58:03 +1000

    made it work

--- build.sh
+++ build.sh
@@ -2,8 +2,8 @@ nasm -f bin -o boot.bin asm/boot.asm
 nasm -f bin -o loader.bin asm/loader.asm
 nasm -f elf64 -o kernel.o asm/kernel.asm
 nasm -f elf64 -o trapa.o asm/trap.asm
-cargo rustc -- --emit obj  -C link-arg=-nostartfiles -C no-redzone=yes -O
-ld -nostdlib -T link.lds -o kernel kernel.o trapa.o $(find target/debug/deps -name 'op_system_rs*.o') 
+cargo rustc --bin op_system_rs --  --emit obj  -C link-arg=-nostartfiles -C no-redzone=yes -O
+ld --allow-multiple-definition -nostdlib -T link.lds -o kernel kernel.o trapa.o $(find target/debug/deps -name 'op_system_rs*.o') 
 objcopy -O binary kernel kernel.bin 
 dd if=boot.bin of=boot.img bs=512 count=1 conv=notrunc
 dd if=loader.bin of=boot.img bs=512 count=5 seek=1 conv=notrunc
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
-#![allow(unused_assignments, dead_code)]
+// #![allow(unused_assignments, dead_code)]
 #![no_std] // don't link the Rust standard library
 #![no_main] // disable all Rust-level entry points
 
+// use op_system_rs::trap;
 mod trap;
 
 // use core::panic::PanicInfo;
@@ -14,13 +15,13 @@ pub extern "C" fn KMain() {
     // named `_start` by default
 
 
-    // trap::create_idt_table();
+    trap::create_idt_table();
 
-    let vga_buffer = 0xb8000 as *mut u8;
-    unsafe {
-        *vga_buffer.offset(0) = 'Z' as u8;
-        *vga_buffer.offset(1) = 0xa;
-    }
+    // let vga_buffer = 0xb8000 as *mut u8;
+    // unsafe {
+    //     *vga_buffer.offset(0) = 'Z' as u8;
+    //     *vga_buffer.offset(1) = 0xa;
+    // }
 
 
     // loop {}
